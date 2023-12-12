005241125e55b043712e3679d4b1882cedde5384
Boyce007 <diboyce21@students.desu.edu>
2023-12-12 20:32:00 +0000

    Pg.c done

--- .h
+++ .h
@@ -2,7 +2,7 @@
 #define TLB_SIZE 16;
 #define PAGE_TABLE_SIZE 256;
 #define NUM_OF_PAGES NUM_OF_FRAMES;
-#define index_to_replace 0;
+#define MEMORY_SIZE 65536
 
 typedef struct {
     int isValid;
@@ -29,6 +29,19 @@ int check_tlb();
 // Page table function 
 void add_to_page_table();
 int check_page_table();
+
+
+// pages functions 
+
+
+int* read_addresses();
+
+int get_page_number(int logical_address);
+
+int get_offsef(int logical_address);
+
+Page* convert_addresses(int* addresses);
+
 
 
 
